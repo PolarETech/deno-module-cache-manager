144a95b7b9dc298a8cc7d56d6bf086b9c0c08799
Polar Tech <52513329+PolarETech@users.noreply.github.com>
2022-03-20 17:56:16 +0900

    feat: display path of cached module files with missing URL (--missing-url option)

--- cli.js
+++ cli.js
@@ -54,6 +54,14 @@ function isFileExist(path) {
   }
 }
 
+function isDirectoryExist(path) {
+  try {
+    return Deno.lstatSync(path).isDirectory;
+  } catch (_e) {
+    return false;
+  }
+}
+
 function buildBaseFilePath(url, hash) {
   url = new URL("", url);
 
@@ -103,6 +111,44 @@ function collectRelatedFilePath(moduleData) {
   }
 
   return moduleData;
+}
+
+// WARNING:
+// This function does not collect compiled files
+// created by Deno v1.2.2 or earlier, whose file names are not hashed.
+// If it eventually becomes necessary, its implementation should be considered separately.
+// https://github.com/denoland/deno/pull/6911
+function collectAllHashedFilePath(type = "") {
+  const baseDirList = (() => {
+    switch (type) {
+      case "modulesCache":
+        return [`${baseDepsPath}/https`, `${baseDepsPath}/http`];
+      case "typescriptCache":
+        return [`${baseGenPath}/https`, `${baseGenPath}/http`];
+      default:
+        return [`${baseDepsPath}/https`, `${baseDepsPath}/http`, `${baseGenPath}/https`, `${baseGenPath}/http`];
+    }
+  })();
+
+  const hostDirList = [];
+  for (const baseDir of baseDirList) {
+    if (isDirectoryExist(baseDir) === false) continue;
+    for (const dirEntry of Deno.readDirSync(baseDir)) {
+      if (dirEntry.isDirectory === false) continue;
+      hostDirList.push(`${baseDir}/${dirEntry.name}`);
+    }
+  }
+
+  const pathList = [];
+  for (const hostDir of hostDirList) {
+    for (const dirEntry of Deno.readDirSync(hostDir)) {
+      if (dirEntry.isDirectory) continue;
+      if (dirEntry.name.startsWith(".")) continue;
+      pathList.push(`${hostDir}/${dirEntry.name}`);
+    }
+  }
+
+  return pathList;
 }
 
 // TODO:
@@ -163,6 +209,32 @@ function displayCachedModuleList(moduleData, withPath = false) {
   }
 }
 
+function displayPathOfFileWithMissingURL() {
+  const pathList = collectAllHashedFilePath();
+
+  let fileCount = 0;
+
+  for (const path of pathList) {
+    if (path.startsWith(baseDepsPath) && path.endsWith(".metadata.json")) continue;
+
+    const adjustedPath = path.replace(/\.d\.ts$|\.js$|\.js\.map$|\.buildinfo$|\.meta$/, "");
+    const depsMetadataFilePath = `${adjustedPath.replace(baseGenPath, baseDepsPath)}.metadata.json`;
+
+    if (isFileExist(depsMetadataFilePath)) continue;
+
+    console.log(path);
+    fileCount++;
+  }
+
+  if (fileCount === 0) {
+    console.log("INFO: No files are found");
+  } else if (fileCount === 1) {
+    console.log(`\nTotal: ${fileCount} file is found`);
+  } else {
+    console.log(`\nTotal: ${fileCount} files are found`);
+  }
+}
+
 function checkDenoVersion(version) {
   const requiredVersion = version.split(".").map((n) => Number(n));
   const currentVersion = Deno.version.deno.split(".").map((n) => Number(n));
@@ -177,6 +249,11 @@ function checkDenoVersion(version) {
 function main() {
   if (checkDenoVersion(requiredMinDenoVer) === false) {
     console.log(`INFO: Deno version ${requiredMinDenoVer} or later is required`);
+    Deno.exit();
+  }
+
+  if (Deno.args.includes("--missing-url")) {
+    displayPathOfFileWithMissingURL();
     Deno.exit();
   }
 
