b757f50cd18fa72d4386abeb8659317aaf215b1a
Polar Tech <52513329+PolarETech@users.noreply.github.com>
2022-07-28 22:20:56 +0900

    refactor: correct typos and update comments about cache file extensions

--- src/missingUrl.ts
+++ src/missingUrl.ts
@@ -57,10 +57,25 @@ export function collectPathOfFileWithMissingURL(): string[] {
 
   const metadataExt = ".metadata.json";
 
-  // REVIEW:
+  // NOTE:
+  // (Past Notes)
   // I have never seen .d.ts file created in the gen/http(s) directory.
-  // Is it necessary to handle the .d.ts extention?
+  // Is it necessary to handle the .d.ts extension?
   // https://github.com/denoland/deno/blob/v1.20.1/cli/cache.rs#L186
+  //
+  // (Update Notes)
+  // .d.ts extension is removed from Deno source code at v1.23.0.
+  // https://github.com/denoland/deno/commit/0b90e966c5e22b95c283a10407234cad37b8f19b
+  //
+  // .buildinfo extension is removed from Deno source code at v1.24.0.
+  // https://github.com/denoland/deno/pull/15118/files#diff-0fab6cf2f78858285fdf1c7991480f8d39f6356900d8e22b2c370909e8d8e917
+  //
+  // In the user's environment, there may exist the cache files
+  // with the above extensions stored in the past.
+  // Therefore, we should not exclude them from the processing.
+  //
+  // TODO:
+  // Extension-related processing is also present in the moduleData.ts file.
   const regexpToRemoveExt = new RegExp(/\.d\.ts$|\.js$|\.js\.map$|\.buildinfo$|\.meta$/);
 
   const pathListWithMissingURL: string[] = [];
--- src/moduleData.ts
+++ src/moduleData.ts
@@ -128,13 +128,28 @@ export class ModuleData {
   }
 
   collectRelatedFilePath(): void {
-    const extentionsInDeps = ["", ".metadata.json"];
+    const extensionsInDeps = ["", ".metadata.json"];
 
-    // REVIEW:
+    // NOTE:
+    // (Past Notes)
     // I have never seen .d.ts file created in the gen/http(s) directory.
-    // Is it necessary to handle the .d.ts extention?
+    // Is it necessary to handle the .d.ts extension?
     // https://github.com/denoland/deno/blob/v1.20.1/cli/cache.rs#L186
-    const extentionsInGen = [".d.ts", ".js", ".js.map", ".buildinfo", ".meta"];
+    //
+    // (Update Notes)
+    // .d.ts extension is removed from Deno source code at v1.23.0.
+    // https://github.com/denoland/deno/commit/0b90e966c5e22b95c283a10407234cad37b8f19b
+    //
+    // .buildinfo extension is removed from Deno source code at v1.24.0.
+    // https://github.com/denoland/deno/pull/15118/files#diff-0fab6cf2f78858285fdf1c7991480f8d39f6356900d8e22b2c370909e8d8e917
+    //
+    // In the user's environment, there may exist the cache files
+    // with the above extensions stored in the past.
+    // Therefore, we should not exclude them from the processing.
+    //
+    // TODO:
+    // Extension-related processing is also present in the missingUrl.ts file.
+    const extensionsInGen = [".d.ts", ".js", ".js.map", ".buildinfo", ".meta"];
 
     for (const url of this.targetedUrlList) {
       const {
@@ -145,12 +160,12 @@ export class ModuleData {
 
       const pathList = [];
 
-      for (const ext of extentionsInDeps) {
+      for (const ext of extensionsInDeps) {
         pathList.push(depsHashedPath + ext);
       }
 
       for (const path of [genHashedPath, genUrlPath]) {
-        for (const ext of extentionsInGen) {
+        for (const ext of extensionsInGen) {
           pathList.push(path + ext);
         }
       }
